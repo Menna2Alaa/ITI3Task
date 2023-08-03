e00859de5df931b9b7f19cd7b220a07c2c40b17d
Menna2Alaa <120278348+Menna2Alaa@users.noreply.github.com>
2023-08-03 16:48:20 +0300

    iti

--- app/src/main/java/com/example/ititask/MainActivity.kt
+++ app/src/main/java/com/example/ititask/MainActivity.kt
@@ -2,20 +2,41 @@ package com.example.ititask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.ititask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private var sport = ""
+    private var gender = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.login.setOnClickListener {
-            println(binding.editText.text)
-            println(binding.edit.text)
-        }
 
+        binding.football.setOnCheckedChangeListener { _, b ->
+            if (b) {
+                sport = "your favorite sport is football"
+            } else if (binding.tennis.isChecked) {
+                sport = "your favorite sport is tennis"
+            } else if (binding.basketball.isChecked) {
+                sport = "your favorite sport is basketball"
+            }
+            binding.male.setOnCheckedChangeListener { _, b ->
+                gender = if (b) {
+                    "you are a male"
+                } else {
+                    "you are a female"
+                }
+            }
+            println(gender)
+            binding.login.setOnClickListener {
+                Toast.makeText(this, "welcome ${binding.editText.text},$sport ,$gender", Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
 }
